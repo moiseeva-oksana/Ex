2bbca2ee73ea103cebec6144372372086e6cc8fb
moiseeva-oksana <moiseeva.ox@gmail.com>
2017-03-07 00:02:40 +0300

    Check refactor

--- Udacity/src/main/java/obj/ObjCheck.java
+++
@@ -1,8 +0,0 @@
-package obj;
-
-public class ObjCheck {
-    public boolean check(int num) {
-        Integer integer = new Integer(num);
-        return Integer.parseInt(integer.toString())==integer.hashCode();
-    }
-}
--- Udacity/src/test/java/obj/ObjCheckTest.java
+++ Udacity/src/test/java/obj/ObjCheckTest.java
@@ -6,11 +6,15 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ObjCheckTest {
+    public static boolean check(int num) {
+        Integer integer = new Integer(num);
+        return Integer.parseInt(integer.toString())==integer.hashCode();
+    }
     @Test
     public void checkTest() throws Exception {
-        assertThat(new ObjCheck().check(20), is(true));
-        assertThat(new ObjCheck().check(1), is(true));
-        assertThat(new ObjCheck().check(35667), is(true));
+        assertThat(check(20), is(true));
+        assertThat(check(1), is(true));
+        assertThat(check(35667), is(true));
     }
 
 }
