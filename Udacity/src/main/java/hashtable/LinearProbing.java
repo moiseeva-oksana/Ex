2cdfe80fd4d34fb560c43e5d2f7f96d9887818fc
Oksana Moiseeva <oksana_moiseeva@epam.com>
2017-03-14 18:35:37 +0300

    heap

--- Udacity/src/main/java/hashtable/LinearProbing.java
+++ Udacity/src/main/java/hashtable/LinearProbing.java
@@ -4,35 +4,35 @@ public class LinearProbing {
     private String[] table = new String[20_000_000];
 
     public int getHashOfString(String s) {
-        return s.hashCode()/1000;
+        return s.hashCode() / 1000;
     }
 
     public void put(String string) {
-        int i= getHashOfString(string);
-        int count=0;
-        while (table[i]!=null) {
+        int i = getHashOfString(string);
+        int count = 0;
+        while (table[i] != null) {
             count++;
             i++;
-            if(i==table.length-1) {
-                i=0;
+            if (i == table.length - 1) {
+                i = 0;
             }
-            if (count==table.length) {
+            if (count == table.length) {
                 throw new IndexOutOfBoundsException("Table is full");
             }
         }
-        table[i]=string;
+        table[i] = string;
     }
 
     public int get(String string) {
-        int i= getHashOfString(string);
-        int count=0;
-        while (table[i]==null || !table[i].equals(string)) {
+        int i = getHashOfString(string);
+        int count = 0;
+        while (table[i] == null || !table[i].equals(string)) {
             count++;
             i++;
-            if(i==table.length-1) {
-                i=0;
+            if (i == table.length - 1) {
+                i = 0;
             }
-            if (count==table.length) {
+            if (count == table.length) {
                 return -1;
             }
         }
@@ -41,10 +41,10 @@ public class LinearProbing {
 
     public void delete(String string) {
         int index = get(string);
-        if(index==-1) {
+        if (index == -1) {
             return;
         }
-        table[index]=null;
+        table[index] = null;
     }
 
 }
---
+++ Udacity/src/test/java/heap/BinaryHeapTest.java
@@ -0,0 +1,14 @@
+package heap;
+
+import org.junit.Test;
+
+/**
+ * Created by student on 3/14/17.
+ */
+public class BinaryHeapTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+}
